1a390302929092fbf4c6485971d67e7ce320ff92
hamed <hamedgramzi@gmail.com>
2022-03-11 10:15:31 +0330

    fix some bugs after change architecture

--- app/src/main/java/ir/huma/humaleanbackvideolib/MainActivity.kt
+++ app/src/main/java/ir/huma/humaleanbackvideolib/MainActivity.kt
@@ -1,8 +1,6 @@
 package ir.huma.humaleanbackvideolib
 
 import android.Manifest
-import android.app.Activity
-import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.graphics.Typeface
 import android.os.Bundle
@@ -10,84 +8,134 @@ import android.os.Environment
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
+import ir.huma.myExoplayerlib.HumaExoPlayer
 import ir.huma.myExoplayerlib.MediaInfo
-import ir.huma.myExoplayerlib.MyExoPlayerManager
+import ir.huma.myExoplayerlib.HumaExoPlayerView
 import java.io.File
 
 
 class MainActivity : FragmentActivity() {
-    lateinit var myExoPlayer: MyExoPlayerManager
+    lateinit var playerView: HumaExoPlayerView
+    lateinit var player: HumaExoPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) !== PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.RECORD_AUDIO), 102)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) !== PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(Manifest.permission.RECORD_AUDIO),
+                102
+            )
         }
 
-        myExoPlayer = findViewById(R.id.myExoPlayer)
+        playerView = findViewById(R.id.myExoPlayer)
+        player = HumaExoPlayer(this)
+        playerView.player = player
         var rootFile = File(Environment.getExternalStorageDirectory(), "HumaFit");
 //        myExoPlayer.addMediaItem(MediaInfo().addMediaQuality("", "file://"+rootFile.absoluteFile+"/285ccb9c-e2be-4ee8-97fc-64cc5fc27f3a.mp4"))
 //
-        myExoPlayer.addMediaItem(
-                MediaInfo().setTitle("تهران عاشق")
-                        .setLogoUrl("https://music-fa.com/wp-content/uploads/2020/12/Mohammad-Motamedi-Tehrane-Ashegh-Cover-Music-fa.com_-1.jpg")
-                        .setBackgroundUrl("https://music-fa.com/wp-content/uploads/2020/12/Mohammad-Motamedi-Tehrane-Ashegh-Cover-Music-fa.com_-1.jpg")
-                        .setDescription("آلبوم تهران عاشق محمد معتمدی به صورت تکی و یکجا")
-                        .addMediaQuality("128", "https://dls.music-fa.com/tagdl/99/Mohammad%20Motamedi%20-%20Gole%20Sang%20(320).mp3")
+
+        player.addMedia(
+            MediaInfo().setTitle("تهران عاشق")
+                .setLogoUrl("https://music-fa.com/wp-content/uploads/2020/12/Mohammad-Motamedi-Tehrane-Ashegh-Cover-Music-fa.com_-1.jpg")
+                .setBackgroundUrl("https://music-fa.com/wp-content/uploads/2020/12/Mohammad-Motamedi-Tehrane-Ashegh-Cover-Music-fa.com_-1.jpg")
+                .setDescription("آلبوم تهران عاشق محمد معتمدی به صورت تکی و یکجا")
+                .addMediaQuality(
+                    "128",
+                    "https://dls.music-fa.com/tagdl/99/Mohammad%20Motamedi%20-%20Gole%20Sang%20(320).mp3"
+                )
         )
 
-        myExoPlayer.addMediaItem(MediaInfo().setTitle("دیرین دیرین").setSeek(20000)
+        player.addMedia(
+            MediaInfo().setTitle("دیرین دیرین").setSeek(20000)
                 .setDescription(
-                        "https://www.forbes.com/sites/laurashin/2017/02/21/this-is-the-worlds-first-cryptocurrency-issued-by-a-hedge-fund/#6de7a9b560b6\n" +
-                                "\n" +
-                                "https://futurism.com/a-new-startup-just-proved-that-blockchain-is-going-to-utterly-transform-osadd\n\n"
+                    "https://www.forbes.com/sites/laurashin/2017/02/21/this-is-the-worlds-first-cryptocurrency-issued-by-a-hedge-fund/#6de7a9b560b6\n" +
+                            "\n" +
+                            "https://futurism.com/a-new-startup-just-proved-that-blockchain-is-going-to-utterly-transform-osadd\n\n"
                 )
                 .setLogoUrl("https://cdn.isna.ir/d/2017/05/03/3/57478643.jpg?ts=1498045331768")
-                .addMediaQuality("144p", "https://as9.cdn.asset.aparat.com/aparat-video/ab6d4bed0e8142d8a2648dc7cce4af6727728814-144p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImM4N2JiY2JhYjgxNDk2YWFkNWJhYWM4OGRmZTU2N2M3IiwiZXhwIjoxNjA3NTE3NTM5LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.QpofxWAV4uwu0UC8liXbeVRNgv1cyTO9vwjojP9Vt64")
-                .addMediaQuality("240p", "https://as4.cdn.asset.aparat.com/aparat-video/ab6d4bed0e8142d8a2648dc7cce4af6727728814-240p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImIwZDUyYjBjYzcxZGUwYTdjYTk4MzlkMmFjNTE0OTA5IiwiZXhwIjoxNjA3NTM3Mjk5LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.t8FENFALXpjBewE6UVuIWj_abEmsPAu9OlT0kmAUZ3o")
-                .addMediaQuality("360p", "https://as4.cdn.asset.aparat.com/aparat-video/ab6d4bed0e8142d8a2648dc7cce4af6727728814-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImIwZDUyYjBjYzcxZGUwYTdjYTk4MzlkMmFjNTE0OTA5IiwiZXhwIjoxNjA3NTM3Mjk5LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.t8FENFALXpjBewE6UVuIWj_abEmsPAu9OlT0kmAUZ3o")
-                .addMediaQuality("480p", "https://as9.cdn.asset.aparat.com/aparat-video/ab6d4bed0e8142d8a2648dc7cce4af6727728814-480p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6Ijg0NTU0ZjM3NWUwYTBkZjlhMDQ3MDg3NDRiNjcwY2Q3IiwiZXhwIjoxNjA3NTI3MjIxLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.VDqPiZI6DFwopLQFJ8sxJ9lD036YoqlBSKnx0Zv8yXU")
+                .addMediaQuality(
+                    "144p",
+                    "https://as9.cdn.asset.aparat.com/aparat-video/ab6d4bed0e8142d8a2648dc7cce4af6727728814-144p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImM4N2JiY2JhYjgxNDk2YWFkNWJhYWM4OGRmZTU2N2M3IiwiZXhwIjoxNjA3NTE3NTM5LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.QpofxWAV4uwu0UC8liXbeVRNgv1cyTO9vwjojP9Vt64"
+                )
+                .addMediaQuality(
+                    "240p",
+                    "https://as4.cdn.asset.aparat.com/aparat-video/ab6d4bed0e8142d8a2648dc7cce4af6727728814-240p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImIwZDUyYjBjYzcxZGUwYTdjYTk4MzlkMmFjNTE0OTA5IiwiZXhwIjoxNjA3NTM3Mjk5LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.t8FENFALXpjBewE6UVuIWj_abEmsPAu9OlT0kmAUZ3o"
+                )
+                .addMediaQuality(
+                    "360p",
+                    "https://as4.cdn.asset.aparat.com/aparat-video/ab6d4bed0e8142d8a2648dc7cce4af6727728814-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6ImIwZDUyYjBjYzcxZGUwYTdjYTk4MzlkMmFjNTE0OTA5IiwiZXhwIjoxNjA3NTM3Mjk5LCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.t8FENFALXpjBewE6UVuIWj_abEmsPAu9OlT0kmAUZ3o"
+                )
+                .addMediaQuality(
+                    "480p",
+                    "https://as9.cdn.asset.aparat.com/aparat-video/ab6d4bed0e8142d8a2648dc7cce4af6727728814-480p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6Ijg0NTU0ZjM3NWUwYTBkZjlhMDQ3MDg3NDRiNjcwY2Q3IiwiZXhwIjoxNjA3NTI3MjIxLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.VDqPiZI6DFwopLQFJ8sxJ9lD036YoqlBSKnx0Zv8yXU"
+                )
 //                .addSubtitle("https://cloudspace.huma.ir/s/mrziWnyAM4sEimw/download","en")
 //                .addSubtitle("https://cloudspace.huma.ir/s/68q8tKu1I9XTiXg/download", "fa")
 //                .addSubtitle("https://cloudspace.huma.ir/s/ruRcEJ12X3806Df/download", "en")
         )
-        myExoPlayer.addMediaItem(MediaInfo().setSeek(10000).setTitle("طرز تهیه نان سیمیت")
-                .setDescription("آرد، ۵۰۰ گرم\n" +
-                        "خمیرمایه فوری، ۱ قاشق چای\u200Cخوری\n" +
-                        "شکر، ۱ قاشق غذاخوری\n" +
-                        "روغن مایع، ۳ قاشق غذاخوری\n" +
-                        "شیر ولرم، ۳ قاشق غذاخوری\n" +
-                        "آب ولرم، ۱/۴ پیمانه\n" +
-                        "تخم\u200Cمرغ، ۱ عدد\n" +
-                        "نمک، ۲ پنس\n" +
-                        "شیره خرما، ۴ قاشق غذاخوری\n" +
-                        "آب، ۳ قاشق غذاخوری    ")
+        player.addMedia(
+            MediaInfo().setSeek(10000).setTitle("طرز تهیه نان سیمیت")
+                .setDescription(
+                    "آرد، ۵۰۰ گرم\n" +
+                            "خمیرمایه فوری، ۱ قاشق چای\u200Cخوری\n" +
+                            "شکر، ۱ قاشق غذاخوری\n" +
+                            "روغن مایع، ۳ قاشق غذاخوری\n" +
+                            "شیر ولرم، ۳ قاشق غذاخوری\n" +
+                            "آب ولرم، ۱/۴ پیمانه\n" +
+                            "تخم\u200Cمرغ، ۱ عدد\n" +
+                            "نمک، ۲ پنس\n" +
+                            "شیره خرما، ۴ قاشق غذاخوری\n" +
+                            "آب، ۳ قاشق غذاخوری    "
+                )
 
-                .addMediaQuality("144p", "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-144p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg")
-                .addMediaQuality("240p", "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-240p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg")
-                .addMediaQuality("360p", "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg")
-                .addMediaQuality("480p", "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-480p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg")
-                .addMediaQuality("720p", "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-720p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg")
+                .addMediaQuality(
+                    "144p",
+                    "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-144p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg"
+                )
+                .addMediaQuality(
+                    "240p",
+                    "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-240p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg"
+                )
+                .addMediaQuality(
+                    "360p",
+                    "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-360p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg"
+                )
+                .addMediaQuality(
+                    "480p",
+                    "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-480p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg"
+                )
+                .addMediaQuality(
+                    "720p",
+                    "https://hw18.cdn.asset.aparat.com/aparat-video/4ed9428eeb3ee5cde21cc2878860215627721511-720p.mp4?wmsAuthSign=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbiI6IjBjYWJhN2NlYTE2YjY5MmYzMGZmMjU4MWNlYzE3NGYxIiwiZXhwIjoxNjA3NTM3NTAwLCJpc3MiOiJTYWJhIElkZWEgR1NJRyJ9.0CX-RXwDPnd8bsL1X3JzWBVoUuI-Ai-xbOswffh6CMg"
+                )
                 .setCurrentQuality("360p")
         )
-        myExoPlayer.addMediaItem(
-                MediaInfo().setTitle("تست پخش زنده").setLive(true).addMediaQuality("", "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8")
+        player.addMedia(
+            MediaInfo().setTitle("تست پخش زنده").setLive(true).addMediaQuality(
+                "",
+                "http://demo.unified-streaming.com/video/tears-of-steel/tears-of-steel.ism/.m3u8"
+            )
         )
 
 
-        myExoPlayer.typeface = Typeface.createFromAsset(assets, "fonts/BYekan.ttf")
-        myExoPlayer.start()
+        playerView.typeface = Typeface.createFromAsset(assets, "fonts/BYekan.ttf")
+        player.start()
     }
 
     override fun onPause() {
         super.onPause()
-        myExoPlayer.pause()
+        player.pause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        myExoPlayer.release()
+        player.release()
     }
 
 }
---
+++ exoplayerlib/src/main/java/ir/huma/myExoplayerlib/HumaExoPlayer.kt
@@ -0,0 +1,176 @@
+package ir.huma.myExoplayerlib
+
+import android.content.Context
+import android.media.session.PlaybackState
+import android.util.Log
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.*
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+
+class HumaExoPlayer(context: Context) : SimpleExoPlayer(SimpleExoPlayer.Builder(context)) {
+
+    private var dataSourceFactoryHttp: DataSource.Factory =
+        DefaultHttpDataSourceFactory(Util.getUserAgent(context, context.packageName))
+    private var dataSourceFactoryFile: DataSource.Factory = DefaultDataSourceFactory(context)
+    private var qualityChanging = false
+
+    var updateViewListener: UpdateViewListener? = null
+    var mediaInfoes = ArrayList<MediaInfo>()
+        private set
+    var defaultQuality: String? = null
+        get() {
+            if (field == null && mediaInfoes.size > 0) {
+                field = mediaInfoes[0].currentQuality
+            }
+            return field
+        }
+    private var eventListener = object : Player.EventListener {
+        override fun onPlaybackStateChanged(state: Int) {
+            if (state == PlaybackState.STATE_PLAYING) {
+                val item = getCurrentMedia()
+
+                updateViewListener?.update(this@HumaExoPlayer, item!!)
+                defaultQuality = item!!.currentQuality
+                qualityChanging = false
+
+            }
+        }
+
+        override fun onTracksChanged(
+            trackGroups: TrackGroupArray,
+            trackSelections: TrackSelectionArray
+        ) {
+            val item = getCurrentMedia()
+
+            if (item!!.hasVideo == null) {
+                if (trackGroups.length == 1 && trackGroups[0].getFormat(0).sampleMimeType?.indexOf("audio") != -1
+                ) {
+                    item!!.hasVideo = false
+                } else if (trackGroups.length > 1) {
+                    item!!.hasVideo = true
+                }
+            }
+//                super.onTracksChanged(trackGroups, trackSelections)
+            if (!qualityChanging) {
+                updateViewListener?.update(this@HumaExoPlayer, item)
+
+                Log.d(
+                    "exo_player",
+                    "setData" + " : onTrackChanged " + item.title + " " + item.currentQuality
+                )
+                if (item.seek > currentPosition) {
+                    seekTo(item.seek)
+                }
+            }
+        }
+
+    }
+
+    init {
+        addListener(this.eventListener)
+    }
+
+
+    fun notifyMediaChange() {
+        qualityChanging = true
+        pause()
+        stop()
+        clearMediaItems()
+        val media = getCurrentMedia()
+        media!!.getMediaItem(defaultQuality!!)?.let {
+            for (item in mediaInfoes) {
+                addMediaSource(buildMediaSource(item))
+            }
+            seekToDefaultPosition(currentWindowIndex)
+            prepare()
+            play()
+            if (currentPosition > media!!.seek)
+                seekTo(currentPosition)
+            else
+                seekTo(media!!.seek)
+        }
+    }
+
+    fun getCurrentMedia(): MediaInfo? {
+        return currentWindowIndex.let { mediaInfoes.getOrNull(it) }
+    }
+
+
+    fun addMedia(index: Int, mediaInfo: MediaInfo) {
+        mediaInfoes.add(index, mediaInfo)
+        addMediaSource(index, buildMediaSource(mediaInfo))
+    }
+
+    fun addMedia(mediaInfo: MediaInfo) {
+        mediaInfoes.add(mediaInfo)
+        addMediaSource(buildMediaSource(mediaInfo))
+    }
+
+    fun updateMedia(index: Int, media: MediaInfo) {
+        mediaInfoes[index] = media
+        buildMediaSource(media).let {
+            removeMediaItem(index)
+            addMediaSource(index, it)
+        }
+    }
+
+
+    fun addMedias(index: Int, mediaInfoes: List<MediaInfo>) {
+        this.mediaInfoes.addAll(index, mediaInfoes)
+        for (item in mediaInfoes) {
+            addMediaSource(index, buildMediaSource(item))
+        }
+    }
+
+    fun addMedias(mediaInfoes: List<MediaInfo>) {
+        this.mediaInfoes.addAll(mediaInfoes)
+        for (item in mediaInfoes) {
+            addMediaSource(buildMediaSource(item))
+        }
+    }
+
+    private fun buildMediaSource(mediaInfo: MediaInfo): MediaSource {
+        val dataSourceFactory =
+            if (!mediaInfo.isMediaLocal(defaultQuality!!)) dataSourceFactoryHttp else dataSourceFactoryFile
+        val mediaSource: MediaSource = if (!mediaInfo.isLive) {
+            ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(mediaInfo.getMediaItem(defaultQuality!!)!!)
+        } else
+            HlsMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(mediaInfo.getMediaItem(defaultQuality!!)!!)
+        if (mediaInfo.getCurrentSubtitle() == null) {
+            return mediaSource
+        }
+        val subtitleSource: MediaSource = SingleSampleMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(mediaInfo.getCurrentSubtitle()!!, C.TIME_UNSET)
+        val subtitleSource2: MediaSource = SingleSampleMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(mediaInfo.getCurrentSubtitle()!!, C.TIME_UNSET)
+
+        return MergingMediaSource(mediaSource, subtitleSource, subtitleSource2)
+    }
+
+    fun start(currentIndex: Int =0 ) {
+        Log.d("MyExoPlayer", "start")
+
+        seekToDefaultPosition(currentIndex)
+        if (getCurrentMedia()!!.seek > currentPosition) {
+            seekTo(getCurrentMedia()!!.seek)
+        }
+
+        prepare()
+        play()
+    }
+
+
+    interface UpdateViewListener {
+        fun update(player: SimpleExoPlayer, mediaInfo: MediaInfo)
+    }
+
+}
